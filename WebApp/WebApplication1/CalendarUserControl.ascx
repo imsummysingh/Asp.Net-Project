9fa52b5540712e48b8bca3836f6ab6415f73d648
Summy Singh <summysingh86@gmail.com>
2020-12-04 01:45:17 +0530

    web prog added

---
+++ WebApp/WebApplication1/CalendarUserControl.ascx
@@ -0,0 +1,7 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="CalendarUserControl.ascx.cs" Inherits="WebApplication1.CalendarUserControl" %>
+<asp:TextBox ID="txtDate" runat="server" Width="115px"></asp:TextBox>
+<asp:ImageButton ID="ImgBtn" runat="server" 
+    ImageUrl="~/Images/Calendar.png" AlternateText="icon" onclick="ImgBtn_Click" />
+<asp:Calendar ID="Calendar1" runat="server" onselectionchanged="Calendar1_SelectionChanged">
+</asp:Calendar>
+
---
+++ WebApp/WebApplication1/CalendarUserControl.ascx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class CalendarUserControl : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Calendar1.Visible = false;
+            }
+        }
+        public string SelectedDate
+        {
+            get
+            {
+                return txtDate.Text;
+            }
+            set
+            {
+                txtDate.Text = value;
+            }
+        }
+        protected void ImgBtn_Click(object sender, ImageClickEventArgs e)
+        {
+            if (Calendar1.Visible)
+            {
+                Calendar1.Visible = false;
+            }
+            else
+            {
+                Calendar1.Visible = true;
+            }
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            txtDate.Text = Calendar1.SelectedDate.ToShortDateString();
+            Calendar1.Visible = false;
+        }
+    }
+}
