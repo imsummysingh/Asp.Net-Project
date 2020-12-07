7793e50203120c313668e9d19f411c941683b3da
Summy Singh <summysingh86@gmail.com>
2020-12-07 13:34:33 +0530

    singleton added

---
+++ WebApp/SingletonThread/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => PrintStudentdetails(),
+                () => PrintEmployeeDetails()
+                );
+            Console.ReadLine();
+        }
+
+        private static void PrintEmployeeDetails()
+        {
+            /*
+             * Assuming Singleton is created from employee class
+             * we refer to the GetInstance property from the Singleton class
+             */
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PrintDetails("From Employee");
+        }
+
+        private static void PrintStudentdetails()
+        {
+            /*
+                         * Assuming Singleton is created from student class
+                         * we refer to the GetInstance property from the Singleton class
+                         */
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+        }
+    }
+}
